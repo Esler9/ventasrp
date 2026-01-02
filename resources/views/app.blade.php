37a27d47a7dcfc3170b6951836bb35043fbd3f9b
Esler Escobar <escobarcarpio@gmail.com>
2026-01-02 17:15:23 -0600

    asd

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="{{ asset('logos/iconofm.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('logos/iconofm.png') }}">
+    <link rel="shortcut icon" href="{{ asset('logos/iconofm.png') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha384-Piv4xVNRyMGpqkM0Myd7sQJ2DqJE50E+I4QJ9bAQQ4K5j0vvVuv5QW7HQwZ0p5Gy" crossorigin="anonymous">
     @vite('resources/js/pos-app.js')
     @inertiaHead
