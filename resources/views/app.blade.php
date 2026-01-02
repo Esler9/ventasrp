75a36083c7031b2b5dffb836caea1e33e582a10b
Esler Escobar <escobarcarpio@gmail.com>
2026-01-02 17:41:09 -0600

    asdasd

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,9 +6,9 @@
     <link rel="manifest" href="{{ asset('manifest.webmanifest') }}">
     <meta name="theme-color" content="#0f172a">
     <link rel="icon" type="image/svg+xml" href="{{ asset('logos/iconofm.svg') }}">
-    <link rel="icon" type="image/png" href="{{ asset('logos/iconofm.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('logos/iconofm-32.png') }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('logos/iconofm.png') }}">
     <link rel="apple-touch-icon" href="{{ asset('logos/iconofm.png') }}">
-    <link rel="shortcut icon" href="{{ asset('logos/iconofm.png') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha384-Piv4xVNRyMGpqkM0Myd7sQJ2DqJE50E+I4QJ9bAQQ4K5j0vvVuv5QW7HQwZ0p5Gy" crossorigin="anonymous">
     @vite('resources/js/pos-app.js')
     @inertiaHead
