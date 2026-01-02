85ea031e4e04b0730190f7d872feff93eb03b9ba
Esler Escobar <escobarcarpio@gmail.com>
2026-01-02 17:27:58 -0600

    asdad

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="{{ asset('manifest.webmanifest') }}">
+    <meta name="theme-color" content="#0f172a">
     <link rel="icon" type="image/svg+xml" href="{{ asset('logos/iconofm.svg') }}">
     <link rel="icon" type="image/png" href="{{ asset('logos/iconofm.png') }}">
     <link rel="apple-touch-icon" href="{{ asset('logos/iconofm.png') }}">
